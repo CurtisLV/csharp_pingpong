816b462ec8a37f11c928a36e4d74bb8a55df7934
CurtisLV <curtislv@gmail.com>
2023-04-16 13:13:56 +0300

    16.04. 4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pong));
             this.SuspendLayout();
             // 
             // Pong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::csharp_pingpong.Properties.Resources.Pong_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1158, 678);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pong";
             this.Text = "Pong";
             this.Load += new System.EventHandler(this.Form1_Load);
