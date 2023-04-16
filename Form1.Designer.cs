5db9a28e9180f7902d9f7e8e8a93b3d3b29e998e
CurtisLV <curtislv@gmail.com>
2023-04-16 13:15:58 +0300

    16.04. 5

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pong));
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.cpuPlayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.player1.Location = new System.Drawing.Point(12, 215);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(22, 159);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // cpuPlayer
+            // 
+            this.cpuPlayer.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.cpuPlayer.Location = new System.Drawing.Point(1124, 215);
+            this.cpuPlayer.Name = "cpuPlayer";
+            this.cpuPlayer.Size = new System.Drawing.Size(22, 159);
+            this.cpuPlayer.TabIndex = 1;
+            this.cpuPlayer.TabStop = false;
             // 
             // Pong
             // 
@@ -38,15 +60,22 @@
             this.BackgroundImage = global::csharp_pingpong.Properties.Resources.Pong_Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1158, 678);
+            this.Controls.Add(this.cpuPlayer);
+            this.Controls.Add(this.player1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pong";
             this.Text = "Pong";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox player1;
+        private PictureBox cpuPlayer;
     }
 }
