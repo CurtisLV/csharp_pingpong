011c81a3c2c4cfbcf4d17d994c7dcc8a1f207e94
CurtisLV <curtislv@gmail.com>
2023-04-17 19:22:56 +0300

    17.04. 9

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -118,6 +118,7 @@
             this.Controls.Add(this.player1);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pong";
             this.Text = "Pong";
