36a084b74d74777d999c1b38fd1998366bcea3f4
CurtisLV <curtislv@gmail.com>
2023-04-21 17:17:20 +0300

    21.04. 11

--- CharacterForm.cs
+++ CharacterForm.cs
@@ -28,17 +28,29 @@ namespace csharp_pingpong
 
         private void secondFace_Click(object sender, EventArgs e)
         {
-            //
+            (Owner as Pong).pongBall.Image = Image.FromFile(
+                @"C:\Users\karli\Desktop\GitHub\csharp_pingpong\MOG.png"
+            );
+            (Owner as Pong).pongTimer.Start();
+            this.Hide();
         }
 
         private void thirdFace_Click(object sender, EventArgs e)
         {
-            //
+            (Owner as Pong).pongBall.Image = Image.FromFile(
+                @"C:\Users\karli\Desktop\GitHub\csharp_pingpong\MOG.png"
+            );
+            (Owner as Pong).pongTimer.Start();
+            this.Hide();
         }
 
         private void fourthFace_Click(object sender, EventArgs e)
         {
-            //
+            (Owner as Pong).pongBall.Image = Image.FromFile(
+                @"C:\Users\karli\Desktop\GitHub\csharp_pingpong\MOG.png"
+            );
+            (Owner as Pong).pongTimer.Start();
+            this.Hide();
         }
     }
 }
